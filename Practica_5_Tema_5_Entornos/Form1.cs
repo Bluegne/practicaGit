e5f771f4573aa28c86d0029e1a7a3cd43f53805f
AsisAlla <asialllla@alu.edu.gva.es>
2023-11-23 17:17:25 +0100

    Corregidos errores en la práctica

--- Practica_5_Tema_5_Entornos/Form1.cs
+++ Practica_5_Tema_5_Entornos/Form1.cs
@@ -22,29 +22,32 @@ namespace Practica_5_Tema_5_Entornos
             string textoTelegrama;
             char tipoTelegrama = ' ';
             int numPalabras = 0;
-            double coste;
+            double coste = 0;
             //Leo el telegrama
             textoTelegrama = txtTelegrama.Text;
             // telegrama urgente?
             if (cbUrgente.Checked)
                 tipoTelegrama = 'u';
+            //añado la condición si no es urgente
+            else
+            {
+                tipoTelegrama = 'o';
+            }
             //Obtengo el número de palabras que forma el telegrama
-            numPalabras = textoTelegrama.Length;
+            numPalabras = textoTelegrama.Split(' ').Length;
             //Si el telegrama es ordinario
             if (tipoTelegrama == 'o')
                 if (numPalabras <= 10)
-                    coste = 25;
+                    coste = 2.5;
                 else
-                    coste = 0.5 * numPalabras;
+                    coste = 2.5 + 0.5 * (numPalabras - 10);
             else
             //Si el telegrama es urgente
             if (tipoTelegrama == 'u')
                 if (numPalabras <= 10)
                     coste = 5;
                 else
-                    coste = 5 + 0.75 * (numPalabras - 10);
-            else
-                coste = 0;
+                    coste = 5 + 0.75 * (numPalabras - 10);           
             txtPrecio.Text = coste.ToString() + " euros";
         }
     }
